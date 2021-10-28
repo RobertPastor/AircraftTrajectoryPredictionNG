2e8010653c553c0ff68a6a72dd9268838595a671
Robert <robert.pastor0691@orange.fr>
2021-10-28 14:29:04 +0200

    update opimization four

--- Home/AirlineOptimization/OptimizationFour.py
+++ Home/AirlineOptimization/OptimizationFour.py
@@ -101,13 +101,14 @@ class TestMethods(unittest.TestCase):
             print ("index= {0} - Airline City= {1}".format(index, listOfCities[i]))
             index = index + 1
             
+        print ( ''' ----------- departure airports - flight leg origin ----------- ''' )
         listOfDepartureAirports =  airlineRoutesAirports.getDepartureAirportsICAOcodeList()
         index = 1
         for l in range(len(listOfDepartureAirports)):
             print ("index= {0} - Departure Airport= {1}".format(index, listOfDepartureAirports[l]))
             index = index + 1
             
-            
+        print ( ''' ----------- arrival airports - flight leg destination  ----------- ''' )
         listOfArrivalAirports =  airlineRoutesAirports.getArrivalAirportsICAOcodeList()
         index = 1
         for l in range(len(listOfArrivalAirports)):
@@ -118,18 +119,16 @@ class TestMethods(unittest.TestCase):
         print('------------ daily hour minutes schedule -------------')
         dailyHoursMinutes = []
         dailyMinutesSpan = 2360
-        for t in range(dailyMinutesSpan):
-            #print ( i )
-            dailyHoursMinutes.append( t )
+        for hour in range(24):
+            #print ( str(hour) )
+            #print ( str(hour).zfill(2) )
+            for minute in range(60):
+                #print ( minute )
+                #print ( str(minute).zfill(2) )
+                #print ( str(hour).zfill(2) +  str(minute).zfill(2) )
+                dailyHoursMinutes.append( str(hour).zfill(2) + ":" +  str(minute).zfill(2) )
             
-        departureTime = {}
-        for k in range(len(airlineAircraftICAOcodeList)):
-            ''' 0800 - 800 means 8 o'clock '''
-            departureTime[k] = 800
-            
-        arrivalTime = {}
-        for k in range(len(airlineAircraftICAOcodeList)):
-            arrivalTime[k] = 1300
+
             
         print ("-----------airline routes costs---------")
 
@@ -137,7 +136,10 @@ class TestMethods(unittest.TestCase):
         airlineCosts_np_array = airlineAircraftRoutesCosts.read()
         print ( airlineCosts_np_array )
         self.assertTrue( airlineCosts_np_array is not None )
-
+        
+        
+        print (''' nodes [ k , i ] is the set of times when an arrival or a departure of an aircraft of type k may happen in the city i ''')
+        
 
         print ( " ----------- create the costs table ------------------ ")
         costs = []
@@ -179,26 +181,35 @@ class TestMethods(unittest.TestCase):
         
         airlineTurnAroundTimesDatabase = AirlineTurnAroundTimesDatabase()
         
+        turnAroundTimeInSeconds = {}
         for k in range(len(airlineAircraftICAOcodeList)):
             aircraftICAOcode = airlineAircraftICAOcodeList[k]
             print ( aircraftICAOcode )
             for i in range(len(listOfCities)):
                 print ("Airline City= {0}".format( listOfCities[i]))
                 airportICAOcode = listOfCities[i]
-                turnAroundTimeInSeconds = airlineTurnAroundTimesDatabase.getTurnAroundTimeInSeconds( aircraftICAOcode , airportICAOcode )
+                turnAroundTimeInSeconds[k , i] = airlineTurnAroundTimesDatabase.getTurnAroundTimeInSeconds( aircraftICAOcode , airportICAOcode )
                 #print ("turn around time in seconds = {0}".format( turnAroundTimeInSeconds ) )
 
         print ( " ----------- aircraft available instances  ------------------ ")
+        fleetSize = {}
         for k in range(len(airlineAircraftICAOcodeList)):
             aircraftICAOcode = airlineAircraftICAOcodeList[k]
         
             nbAircraftsAvailable = airlineFleet.getAircraftNumberOfInstances( aircraftICAOcode )
             print ("Number of available aircrafts for aircraft ICAO code= {0} - nb instances = {1}".format( aircraftICAOcode , nbAircraftsAvailable ) ) 
-
+            fleetSize[k] = nbAircraftsAvailable
 
         ''' ------------- departure time of each flight leg -------- '''
+        flightLegDepartureTime = {}
         for l in range(len(flightLegsList)):
-            
+            flightLegDepartureTime[l] = str("0800")
+        
+        
+        flightLegArrivalTime = {}
+        for l in range(len(flightLegsList)):
+            flightLegArrivalTime[l] = str("1300")
+
 
 
         ''' derived sets to be completed '''
@@ -210,49 +221,68 @@ class TestMethods(unittest.TestCase):
 
         ''' ------------ count air -------------'''
         ''' capture the set of cities where an aircraft of type k might be in the air at midnight '''
-        count_air = {}
+        countAircraftTypeInTheAir = {}
         for k in range(len(airlineAircraftICAOcodeList)):
             pass
-
+            countAircraftTypeInTheAir[k] = 0
 
         ''' ------------ count ground  -------------'''
         ''' capture the set of cities where an aircraft of type k might be sitting on the ground at midnight  '''
-        count_ground = {}
+        countAircraftTypeOntheGround = {}
         for k in range(len(airlineAircraftICAOcodeList)):
             pass
+            countAircraftTypeOntheGround[k] = 0
 
         ''' ------------ variable ------------'''
         '''  x[i, j] is an array of 0-1 variables, which will be 1 '''
         '''  if worker i is assigned to task j. '''
-        x = {}
+        xFleetAssignments = {}
         for k in range(len(airlineAircraftICAOcodeList)):
             #print ( airlineAircraftICAOcodeList[k])
             #acICAOcode = 
+            aircraftICAOcode = airlineAircraftICAOcodeList[k]
+            nbAircraftsAvailable = airlineFleet.getAircraftNumberOfInstances( aircraftICAOcode )
+
             for l in range(len(flightLegsList)):
                 #print ( flightLegsList[l] )
-                x[k, l] = solver.IntVar(0, 1, '{0}-{1}'.format(airlineAircraftICAOcodeList[k] , flightLegsList[l]))
+                xFleetAssignments[k, l] = solver.IntVar(0, nbAircraftsAvailable, '{0}-{1}'.format(airlineAircraftICAOcodeList[k] , flightLegsList[l]))
+
 
         ''' -------- variable -------- number of aircrafts on the ground '''
         yOnTheGround = {}
         for k in range(len(airlineAircraftICAOcodeList)):
             for i in range(len(listOfCities)):
                 for t in range(dailyMinutesSpan):
-                    yOnTheGround[k, i , t] =  solver.IntVar(0, 999, '{0}-{1}'.format(airlineAircraftICAOcodeList[k] , listOfCities[i] , nodes[k, i]))
+                    
+                    aircraftICAOcode = airlineAircraftICAOcodeList[k]
+                    nbAircraftsAvailable = airlineFleet.getAircraftNumberOfInstances( aircraftICAOcode )
+                    
+                    yOnTheGround[k, i , t] =  solver.IntVar(0, nbAircraftsAvailable, '{0}-{1}'.format(airlineAircraftICAOcodeList[k] , listOfCities[i] , nodes[k, i]))
+
 
         ''' --------- variable -> number of used aircrafts ----------'''
         zUsedAircrafs = {}
         for k in range(len(airlineAircraftICAOcodeList)):
-            zUsedAircrafs[k] = solver.IntVar(0, 999, '{0}'.format(airlineAircraftICAOcodeList[k] ))
+            
+            aircraftICAOcode = airlineAircraftICAOcodeList[k]
+            nbAircraftsAvailable = airlineFleet.getAircraftNumberOfInstances( aircraftICAOcode )
+
+            zUsedAircrafs[k] = solver.IntVar(0, nbAircraftsAvailable, '{0}'.format(airlineAircraftICAOcodeList[k] ))
 
                     
         ''' ----------- constraints ---------- '''
             
         ''' COVER - Each flight leg is assigned to exactly one aircraft '''
             
-        ''' Each flight leg is assigned to exactly one aircraft '''
+        ''' Each flight leg is assigned to exactly one aircraft type '''
         for l in range(len(flightLegsList)):
-            ''' sum of all ONEs for any aircraft different type for one leg is always ONE '''
-            solver.Add(solver.Sum([x[k, l] for k in range(len(airlineAircraftICAOcodeList))]) == 1)
+            for k in range(len(airlineAircraftICAOcodeList)):
+                ''' sum of all ONEs for any aircraft different type for one leg is always ONE '''
+                aircraftICAOcode = airlineAircraftICAOcodeList[k]
+                nbAircraftsAvailable = airlineFleet.getAircraftNumberOfInstances( aircraftICAOcode )
+
+                #solver.Add( xFleetAssignments[k,l]  <= nbAircraftsAvailable )
+                solver.Add( xFleetAssignments[k,l]  == 1 )
 
 
         ''' constraint - for one aircraft type - number of instances are limited '''
@@ -260,13 +290,13 @@ class TestMethods(unittest.TestCase):
         for k in range(len(airlineAircraftICAOcodeList)):
             pass
             #zUsedAircrafs[k] 
-            
+            #solver.Add(solver.Sum( [ xFleetAssignments[k, l] for l in range(len(flightLegsList)) ]  -  sum([sum([ yOnTheGround[k, i , t] for i in range(len(listOfCities)) ]) for t in range(dailyMinutesSpan) ]) )  == zUsedAircrafs[k] )
 
         ''' --------- objective --------------'''
         objective_terms = []
         for i in range(num_aircrafts):
             for j in range(num_flight_legs):
-                objective_terms.append(costs[i][j] * x[i, j])
+                objective_terms.append(costs[i][j] * xFleetAssignments[i, j])
                 
         
         ''' minimize the costs '''
@@ -283,7 +313,8 @@ class TestMethods(unittest.TestCase):
             for k in range(len(airlineAircraftICAOcodeList)):
                 for l in range(len(flightLegsList)):
                     # Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
-                    if x[k, l].solution_value() > 0.5:
+                    print ( xFleetAssignments[k, l] )
+                    if xFleetAssignments[k, l].solution_value() > 0.5:
                         print('aircraft {0} assigned to flight leg {1} - Costs = {2:.2f} in US dollars'.format( k, j, costs[k][l]))
                         print('aircraft {0} - ICAO code {1} - assigned to flight leg {2} - Cost = {3:.2f} US dollars for the flight duration'.format( airlineAircraftFullNameList[k], airlineAircraftICAOcodeList[k], flightLegsList[l], costs[k][l] ) )
                         print('aircraft {0} - ICAO code {1} - assigned to flight leg {2} - number of seats = {3} for the selected aircraft'.format( airlineAircraftFullNameList[k], airlineAircraftICAOcodeList[k], flightLegsList[l], airlineAircraftNumberOfSeatsList[k] ) )
